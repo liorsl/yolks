387a92fcf1de3b863ee9b242da9caea3fc030f03
Lior S <liorsl@protonmail.com>
2021-07-27 20:01:25 +0300

    Got php72 to work

--- apache/php72/entrypoint.sh
+++ apache/php72/entrypoint.sh
@@ -7,8 +7,29 @@ export TZ
 # Set environment variable that holds the Internal Docker IP
 INTERNAL_IP=$(ip route get 1 | awk '{print $NF;exit}')
 export INTERNAL_IP
+echo "Internal ip is $INTERNAL_IP"
 
-cd /home/container
+cd /home/container || exit 1
+
+APACHE_PID_FILE=/home/container/apache2.pid
+export APACHE_PID_FILE
+
+APACHE_DOCUMENT_ROOT=/home/container/wwwdocs
+export APACHE_DOCUMENT_ROOT
+
+# Disable for now since it disabled console logging so there is no way to know when the server have started
+#APACHE_LOG_DIR=/home/container/logs
+#export APACHE_LOG_DIR
+
+if [ -f "apache2.conf" ]; then
+   echo "Found existing apache installation."
+else
+   	echo "Copying configuration files..."
+   cp -a /etc/apache2/* .
+   mkdir wwwdocs
+   chmod +x wwwdocs
+   rm envvars
+fi
 
 # Output php version
 php -v
@@ -22,4 +43,3 @@ echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
 ${MODIFIED_STARTUP}
-
